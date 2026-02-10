d93e72dec5fb7db1d28be4efbab2d631750d6f09
Manish Kumar <mk9784129@gmail.com>
2026-02-10 18:06:05 +0530

    Day 2

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,30 +1,44 @@
+// Import mongoose library
+// Mongoose helps us define structure (schema) for MongoDB documents
 const mongoose = require("mongoose");
 
-const UserSchema  = new mongoose.Schema(
+// Create a Schema (Blueprint of User document)
+const userSchema = new mongoose.Schema(
   {
+    // name field
     name: {
-      type: String,
-      required: true,
+      type: String, // Data type will be String
+      required: true, // This field must be provided while creating user
     },
 
+    // email field
     email: {
-      type: String,
-      required: true,
+      type: String, // Email must be a String
+      required: true, // Cannot create user without email
+      unique: true, // No two users can have same email (adds unique index)
     },
 
+    // password field
     password: {
-      type: String,
-      required: true,
+      type: String, // Will store hashed password (not plain text)
+      required: true, // Password is mandatory
     },
 
+    // role field (for authorization)
     role: {
-      type: String,
-      default: "user",
+      type: String, // Role is stored as string
+      default: "user", // If not provided, it automatically becomes "user"
     },
   },
   {
+    // Automatically adds:
+    // createdAt → when user was created
+    // updatedAt → when user was last updated
     timestamps: true,
   },
 );
 
-module.exports = mongoose.model("User", UserSchema);
+// Export the model
+// "User" → MongoDB collection name will become "users"
+// userSchema → structure of each document
+module.exports = mongoose.model("User", userSchema);
